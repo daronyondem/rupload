87ce7f06054f38c85569490c92f9366e3bfebdc7
Garen Yöndem <garen@yondem.com>
2016-10-30 19:17:05 +0300

    cleaned unused usings

--- rupload/Services/JsonConfigService.cs
+++ rupload/Services/JsonConfigService.cs
@@ -1,10 +1,6 @@
 ﻿using Newtonsoft.Json;
-using rupload.Services.Azure;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace rupload.Services
